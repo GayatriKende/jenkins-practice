b5494ec40744cbe7db8f4273381229bdf71a4929
gayatri <gayatri@DESKTOP-VIMQI9I.localdomain>
2026-03-08 09:11:33 +0000

    D

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+ from flask import Flask
 app= Flask(__name__)
 
 @app.route("/")
 def home():
-      return " Version 10 HUB   !"
+      return " Version 10 HUB  && NEW  !"
 
 if __name__ == "__main__" :
      app.run(host="0.0.0.0", port=5000)
