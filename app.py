53f8346f630fcd513276d76db01096cedca4fd2c
gayatri <gayatri@DESKTOP-VIMQI9I.localdomain>
2026-03-08 09:06:31 +0000

    Docker Hub

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app= Flask(__name__)
 
 @app.route("/")
 def home():
-      return " Version 10 with DOKCER HUB !!"
+      return " Version 10 HUB   !"
 
 if __name__ == "__main__" :
      app.run(host="0.0.0.0", port=5000)
